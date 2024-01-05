4f5a20ce8cb2de8df344ddb94f6ced0520678538
Sunil Prasad Yadav <sunilpro137@gmail.com>
2024-01-05 19:44:11 +0530

    Build authentication service with appwrite

---
+++ Appwrite_MegaBlog/src/Appwrite/auth.js
@@ -0,0 +1,54 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        // call another method
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      // console.log("Error getting current User: ", error);
+      throw error;
+    }
+  }
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default AuthService;
